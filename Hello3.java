9e850d5ff2c811d8c7a3239d2d8d3f7d85a14948
AmondeBong <oms1218ju@gmail.com>
2022-02-26 15:49:10 +0900

    thrid test

---
+++ Hello3.java
@@ -0,0 +1,10 @@
+public class Hello3{
+    public static void main(String[] args){
+        System.out.println(6); //number
+        System.out.println("six"); //String "six" 문자열
+        System.out.println("6"); //String "6" 문자열
+        System.out.println(6*6); // 6*6=36
+        System.out.println("6"+"6"); //66 문지와 문자 결합
+        System.out.println("1111".length()); //4 String의 길이
+    }
+}

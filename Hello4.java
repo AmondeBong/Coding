675cd7b254ed01bad9650930d42479ffe3398745
AmondeBong <oms1218ju@gmail.com>
2022-02-26 15:59:49 +0900

    math methood test

---
+++ Hello4.java
@@ -0,0 +1,15 @@
+public class Hello4{
+    public static void main(String[] args){
+
+        //Operator 연산자  
+        System.out.println(6+2); //8
+        System.out.println(6-2); // 4
+        System.out.println(6*2); // 12
+        System.out.println(6/2); //3
+
+        System.out.println(Math.PI); //3.141592.... 수학 명령어 Math 
+        System.out.println(Math.floor(Math.PI)); // 소수점 내림
+        System.out.println(Math.ceil(Math.PI)); // 소수점 올림
+
+    }
+}

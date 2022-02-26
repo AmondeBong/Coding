010a6b22cb7dfe7776b1f9f21b5d10b61370683d
AmondeBong <oms1218ju@gmail.com>
2022-02-26 16:09:52 +0900

    String type test

---
+++ Hello5.java
@@ -0,0 +1,10 @@
+public class Hello5{
+    public static void main(String[] args){
+        System.out.println("Hello World!!"); // String char의 모음
+        System.out.println('H'); //char 한 글자를 표현하는 데이터 타입
+
+        System.out.println("Hello\nWorld"); // \n 띄어쓰기 new line
+        System.out.println("Hello \"World\" "); // \" 역슬래쉬 뒤에 있는 기호는 기호 그자체
+        
+    }
+}
